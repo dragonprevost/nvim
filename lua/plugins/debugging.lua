5192ee33078d9552d1dd1b20a90200de951a7636
Dragon Prevost <dragon@dkp.io>
2024-02-11 16:46:45 -0800

    Setting up neotest

--- lua/plugins/debugging.lua
+++ lua/plugins/debugging.lua
@@ -1,29 +1,34 @@
 return {
-	"mfussenegger/nvim-dap",
-	dependencies = {
-		"rcarriga/nvim-dap-ui",
-    "leoluz/nvim-dap-go",
-	},
-	config = function()
-		local dap, dapui = require("dap"), require("dapui")
-    
-    require("dap-go").setup()
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "mfussenegger/nvim-dap-python",
+    "rcarriga/nvim-dap-ui",
+    "tpope/vim-fugitive",
+  },
+  config = function()
     require("dapui").setup()
+    require("dap-python").setup(
+      "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+    )
+    local dap, dapui = require("dap"), require("dapui")
+    require("dap-python").test_runner = "pytest"
 
-		dap.listeners.before.attach.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.launch.dapui_config = function()
-			dapui.open()
-		end
-		dap.listeners.before.event_terminated.dapui_config = function()
-			dapui.close()
-		end
-		dap.listeners.before.event_exited.dapui_config = function()
-			dapui.close()
-		end
-		
-    vim.keymap.set("n", "<Leader>bp", dap.toggle_breakpoint, {})
-		vim.keymap.set("n", "<Leader>db", dap.continue, {})
-	end,
+    dap.listeners.before.attach.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated.dapui_config = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited.dapui_config = function()
+      dapui.close()
+    end
+
+    vim.keymap.set("n", "<Leader>bp", ":DapToggleBreakpoint<CR>")
+    vim.keymap.set("n", "<Leader>dc", ":DapContinue<CR>")
+    vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
+    vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+  end,
 }
---
+++ lua/plugins/neo-test.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-neotest/neotest-python",
+    },
+    config = function()
+      require("neotest").setup({
+        adapters = {
+          require("neotest-python")({
+            dap = {
+              justMyCode = false,
+              console = "integratedTerminal",
+            },
+            args = { "--log-level", "DEBUG", "--quiet" },
+            runner = "unittest",
+          })
+        }
+      })
+    end
+  },
+}

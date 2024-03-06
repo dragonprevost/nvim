325fee20ad9917c799ffd7ae787b5eabc79ee22c
Dragon Prevost <dragon@dkp.io>
2024-03-05 18:32:25 -0800

    Updated vim column char position to 89

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,7 +3,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set relativenumber")
-vim.cmd("set colorcolumn=80")
+vim.cmd("set colorcolumn=89")
 vim.g.mapleader = " "
 
 -- Navigate vim panes better

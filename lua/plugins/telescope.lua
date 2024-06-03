15d98040db379889a82df2a1725b6f4daa7676f1
Dragon Prevost <dragon@dkp.io>
2024-06-03 14:37:42 -0700

    Cleaned up telescope init

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -19,17 +19,13 @@ return {
             "%_%_pycache%_%_/*",
             "node%_modules/*",
             "node_modules/*",
-            "terraform/*",
             "product-platform-frontend/*",
           }
         }
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      -- vim.keymap.set("n", "<leader>gr", builtin.lsp_references, {})
-      vim.keymap.set("n", "<leader>fb", function ()
-        	require("telescope").extensions.file_browser.file_browser()
-      end)
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 
       require("telescope").load_extension("ui-select")

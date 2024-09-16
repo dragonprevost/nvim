4774ac38fd5b7c105fec62572043599d8f6f8a04
Dragon Prevost <dragon@dkp.io>
2024-09-16 16:08:14 -0700

    Small update to opts/config/

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,15 +1,5 @@
 return {
   "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "pyright",
-      "pytest",
-      "debugpy",
-      "black",
-      "ruff",
-    }
-  },
-  lazy = false,
   config = function()
     require("mason").setup()
   end,
